a26ddecccf935f3076bc93077f68fd1fba84b44d
vincentgauthier068@gmail.com <vincentgauthier068@gmail.com>
2022-03-23 20:58:47 -0700

    scrape the top 3 subs

--- ticker-scraper-reddit.py
+++ ticker-scraper-reddit.py
@@ -1,55 +1,64 @@
 
+from turtle import clear
 from dotenv import load_dotenv
-#supase
 import os
-import time
-import requests
+from pydantic import conset
 import praw
+from supabase import create_client, Client
 from praw.models import MoreComments
 from supabase import create_client, Client
-import re
-load_dotenv()  # take environment variables from .env.
+from stocksymbol import StockSymbol
 
-#Initialize services
+
+load_dotenv()  # take environment variables from .env.
+#Initialize supabase
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+# Get all tickers and set the as a list
+api_key = os.environ.get("SYMBOL_KEY")
+ss = StockSymbol(api_key)
+symbol_list_us = ss.get_symbol_list(market="US")
+tickers = []
+for i in range(len(symbol_list_us)):
+  tickers.append(symbol_list_us[i]['symbol'])
+tickers_set = set(tickers)
 
-
+#Connect to Reddit API via PRAW
 reddit = praw.Reddit(
     client_id=os.environ.get("CLIENT_ID"),
     client_secret=os.environ.get("SECRET_TOKEN"),
-    user_agent="python_fun by u/relevant-magic-card",
+    user_agent="WSBetter by u/relevant-magic-card",
+    ratelimit_seconds=300
 )
 
+#Get hot submissions from 3 most popular trading subs / ensure submissions have comments
 urls = []
-
-for submission in reddit.subreddit("learnpython").hot(limit=10):
-    urls.append(submission.url)
+url_check = 'comment'
+for submission in reddit.subreddit("wallstreetbets").hot(limit=10):
+    if url_check in submission.url:
+      urls.append(submission.url)
+for submission in reddit.subreddit("stocks").hot(limit=10):
+    if url_check in submission.url:
+      urls.append(submission.url)
+for submission in reddit.subreddit("investing").hot(limit=10):
+    if url_check in submission.url:
+      urls.append(submission.url)
 
 submissions = []
-
 for url in urls:
   submissions.append(reddit.submission(url=url))
 
+#Send to Supabase
+def words_in_string(word_list, a_string):
+    return set(word_list).intersection(a_string.split())
 for submission in submissions:
-  for top_level_comment in submission.comments:
-    if isinstance(top_level_comment, MoreComments):
-        continue
     submission.comments.replace_more(limit=None)
     for comment in submission.comments.list():
-      print(comment.body)
-      time.sleep(1)
+      for word in words_in_string(tickers_set, comment.body):
+        data = supabase.table("ticker_mentions").insert({"ticker": word, 'comment': comment.body }).execute()
 
 
-
-
-## Setup chrome options
-# chrome_options = Options()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# driver = webdriver.Chrome('/root/home/projects/python-ticker-scraper/chromedriver/stable/chromedriver',chrome_options=chrome_options)
 
 
 
---
+++ update-tickers.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+from stocksymbol import StockSymbol
+load_dotenv()  # take environment variables from .env.
+#Initialize supabase
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+## get all tickers
+api_key = os.environ.get("SYMBOL_KEY")
+ss = StockSymbol(api_key)
+symbol_list_us = ss.get_symbol_list(market="US")
+tickers = []
+for i in range(len(symbol_list_us)):
+  tickers.append(symbol_list_us[i]['symbol'])
+#push tickers to supabase
+for i in range(len(tickers)):
+  data = supabase.table("tickers").insert({"symbol":tickers[i]}).execute()
